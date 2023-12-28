bb5afe1729f7d57f6090672a7decda357194fafe
Socheath-Horl <horl.socheath@gmail.com>
2023-12-28 14:25:51 +0700

    Implement Category Viewset

--- drfecommerce/product/views.py
+++ drfecommerce/product/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.serializers import ListSerializer
+from .models import Category
+from .serializers import CategorySerializer
 
-# Create your views here.
+
+class CategoryViewSet(viewsets.ViewSet):
+    """
+    A Simple Viewset for viewing categories
+    """
+
+    queryset = Category.objects.all()
+
+    def list(self, request: Request):
+        serializer: ListSerializer = CategorySerializer(self.queryset, many=True)
+        return Response(serializer.data)
