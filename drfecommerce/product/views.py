58ee61ef9e20c783e46f0080b1e45911eee97f52
Socheath-Horl <horl.socheath@gmail.com>
2023-12-29 12:20:49 +0700

    Implement Category Filter by with Extra Action

--- drfecommerce/product/views.py
+++ drfecommerce/product/views.py
@@ -1,5 +1,6 @@
 from drf_spectacular.utils import extend_schema
 from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.serializers import ListSerializer
@@ -51,6 +52,22 @@ class ProductViewSet(viewsets.ViewSet):
     def list(self, request: Request):
         serializer: ListSerializer = ProductSerializer(
             self.queryset,
+            many=True,
+        )
+        return Response(serializer.data)
+
+    @action(
+        methods=["get"],
+        detail=False,
+        url_path=r"category/(?P<category>\w+)/all",
+        url_name="all",
+    )
+    def list_product_by_category(self, request, category=None):
+        """
+        Endpoint to return products by category
+        """
+        serializer: ListSerializer = ProductSerializer(
+            self.queryset.filter(category__name=category),
             many=True,
         )
         return Response(serializer.data)
